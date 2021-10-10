632f6982c263eb2a35a8ad584b5694cc0e4b6b30
DESKTOP-E7HNB1F\pa21 <pro100pro21@mail.ru>
2021-10-10 23:44:25 +0300

    css изменения

--- src/components/search-panel.js
+++ src/components/search-panel.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const SearchPanel = () => {
-    const searchText = 'Type here to search';
-    const searchStyle = {
-      fontSize: '20px'
-    };
-    
-    return <input style={searchStyle} placeholder={searchText} />
-  }
+import './search-panel.css';
 
-  export default SearchPanel;
+const SearchPanel = () => {
+  return (
+    <input type="text"
+              className="form-control search-input"
+              placeholder="type to search" />
+  );
+};
+
+export default SearchPanel;
--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,24 +1,25 @@
 import React from 'react';
+
 import TodoListItem from './todo-list-item';
 import './todo-list.css';
 
-const TodoList = ({todos}) => {
+const TodoList = ({ todos }) => {
 
-    const elements = todos.map((item) => {
-      const {id, ...itemProps} = item;
-
-      return(
-        <li key={id} className="list-group-item">
-          {<TodoListItem {...itemProps} />}
-        </li>
-      ); 
-    });
+  const elements = todos.map((item) => {
+    const { id, ...itemProps } = item;
 
     return (
-      <ul className="list-group todo-list">
-        {elements}
-      </ul>
+      <li key={id} className="list-group-item">
+        <TodoListItem {...itemProps } />
+      </li>
     );
-  };
+  });
 
-  export default TodoList;
+  return (
+    <ul className="list-group todo-list">
+      { elements }
+    </ul>
+  );
+};
+
+export default TodoList;
