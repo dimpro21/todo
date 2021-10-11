8a089a22bc4cc69e243bd57bd30facaac4819c9c
Prohorov <pro100pro21@mail.ru>
2021-10-11 20:24:05 +0300

    36

--- src/components/item-status-filter/item-status-filter.js
+++ src/components/item-status-filter/item-status-filter.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 import './item-status-filter.css';
 
-const ItemStatusFilter = () => {
-  return (
+export default class ItemStatusFilter extends React.Component{
+  
+  render(){
+    return (
     <div className="btn-group">
       <button type="button"
               className="btn btn-info">All</button>
@@ -11,8 +13,6 @@ const ItemStatusFilter = () => {
               className="btn btn-outline-secondary">Active</button>
       <button type="button"
               className="btn btn-outline-secondary">Done</button>
-    </div>
-  );
-};
-
-export default ItemStatusFilter;
+    </div>);
+  }
+}
